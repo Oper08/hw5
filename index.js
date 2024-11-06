a843bed6615e8ad14567e98e4c702443dd790811
oper <seisenbaianuarr@gmail.com>
2024-11-06 12:32:56 +0500

    commit

---
+++ index.js
@@ -0,0 +1,7 @@
+let num1 = parseFloat(prompt("Бірінші санды енгізіңіз:"));
+let num2 = parseFloat(prompt("Екінші санды енгізіңіз:"));
+let num3 = parseFloat(prompt("Үшінші санды енгізіңіз:"));
+
+let average = (num1 + num2 + num3) / 3;
+
+alert("Енгізілген сандардың орташа арифметикалық мәні: " + average);
